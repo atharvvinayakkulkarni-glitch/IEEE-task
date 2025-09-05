7d2fb1fb87fa0ef5eb1363d8868dfe82e5aab1c4
atharvvinayakkulkarni-glitch <atharvvinayakkulkarni@gmail.com>
2025-09-06 01:08:46 +0530

    Update intersection of list.py

--- intersection of list.py
+++ intersection of list.py
@@ -1,8 +1,11 @@
+# both my lists
 list1=[2,3,5,7,6,3,2,9]
 list2=[5,7,4,8,0,1,3]
 list3=[]
 for i in list1:
   if(i in list2):
+  # if ith element of list 1 in 2 append to 3
     list3.append(i)
+# set function to remove repeated values
 list3=list(set(list3))
 print(list3)
