bd828a46e9239cc2d0945f39d9ce5b2d0196c6f7
atharvvinayakkulkarni-glitch <atharvvinayakkulkarni@gmail.com>
2025-09-06 01:19:51 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,7 +1,9 @@
 n=int(input())
 for i in range(n):
+  # printing spaces for inverted triangle
   for j in range(i,n-1):
       print(" ",end='')
+    # printing triangle of asterisks
   for j in range(i+1):
       print('*',end='')
   print('')
